9259126b93d2b5e7baf0183ed0e71c98372034d5
burakcaglar <burakcaglar@yandex.com>
2020-11-26 17:40:01 +0300

    no script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+/** JS NO SCRIPT START **/
+
+var loadDeferredStyles = function() {
+  var addStylesNode = document.getElementById("deferred-styles");
+  var replacement = document.createElement("div");
+  replacement.innerHTML = addStylesNode.textContent;
+  document.body.appendChild(replacement)
+  addStylesNode.parentElement.removeChild(addStylesNode);
+};
+var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+  webkitRequestAnimationFrame || msRequestAnimationFrame;
+if (raf) raf(function() {
+  window.setTimeout(loadDeferredStyles, 0);
+});
+else window.addEventListener('load', loadDeferredStyles);
+
+/** JS NO SCRIPT END **/
+
 /* IS NAVBAR ITEM ACTIVE? START */
 
 const navLinks = document.querySelectorAll(".nav-item");
